0679429d30416f6d851bd744c4e2dec8c8492e4d
Muhammadsaid-09 <muhammadsaidabdurasulov.2009@gmail.com>
2024-08-13 09:48:00 +0500

    Made some changes

--- django/djang/django_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('',index,name='home'),
-    path('home1',about,name='home1'),
-    path('index',func,name='index'),
-    path('register',regfunc,name='reg_name')
-]
